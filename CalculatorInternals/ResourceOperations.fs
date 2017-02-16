64e1370b75376ef9b36434acce83f5afc9c7a659
Jacob Antoni Hassold <jhdude3@gmail.com>
2017-02-15 21:26:11 -0500

    First production function out. Also redid the types

--- CalculatorInternals/ResourceOperations.fs
+++ CalculatorInternals/ResourceOperations.fs
@@ -1,3 +1,29 @@
 ﻿namespace CalculatorInternals
 
+module CreationOperations =
+    open CalculatorInternals.ResourceTypes
 
+    let makeBricks (operationTime : Time) (clayIn : Clay) (timeIn : Time) =
+        let clayPerOp = 3   // Each factory operation needs 3 clay
+        let bricksPerOp = 2     // Each factory operation produces 2 bricks
+        let pricePerOp = 10
+
+        // First calculate number of cycles
+        let numCycles = rClay clayIn
+                        |> amount
+                        |> aUnits ((/) clayPerOp)
+
+        // Cough up the total cost
+        let totalCost = rClay clayIn
+                        |> price
+                        |> aCurr ((+) 10)
+                        |> (*) numCycles
+
+        let totalBricks = numCycles * bricksPerOp
+                        
+        let timeSpent = numCycles * (uTime operationTime)
+                        |> (+) (uTime timeIn)
+
+        timeSpent, (Bricks {Price = (Currency totalCost) ; Amount = (Units totalBricks) })
+
+        
--- CalculatorInternals/ResourceTypes.fs
+++ CalculatorInternals/ResourceTypes.fs
@@ -1,42 +1,84 @@
 ﻿namespace CalculatorInternals
 
-module ResourceTypes =
-    type BaseResource =
-        | Bauxite
-        | Chalcopyrite
-        | Clay
-        | Coal
-        | CrudeOil
-        | GoldOre
-        | Gravel
-        | Ilmenite
-        | IronOre
-        | Limestone
-        | LithiumOre
-        | QuartzSand
-        | RoughDiamonds
-        | SilverOre
+module ResourceTypes = 
+    type Currency = Currency of int
+    let uCurr (Currency c) = c  // Unwrapper
+    let aCurr f (Currency c) = f c  // Applicator
     
-    type ProcessedResource =
-        | Aluminum
-        | Batteries
-        | Bricks
-        | Concrete
-        | Copper
-        | Electronics
-        | Fertilizer
-        | FossilFuel
-        | Glass
-        | Gold
-        | Insecticides
-        | Jewellery
-        | Lithium
-        | MedicalTechnology
-        | Plastics
-        | ScanDrones
-        | Silicon
-        | Silver
-        | Steel
-        | Titanium
-        | Trucks
-        | Weapons
+    type Units = Units of int
+    let uUnits (Units u) = u    // Unwrapper
+    let aUnits f (Units u) = f u    // Applicator
+    
+    type Time = Time of int
+    let uTime (Time t) = t   // Unwrapper
+    let aTime f (Time t) = f t   // Applicator
+    
+    type TotalCost = 
+        { MoneyCost : Currency
+          TimeCost : Time }
+    
+    type Resource = 
+        { Price : Currency
+          Amount : Units }
+    
+    let amount r =
+        r.Amount
+
+    let price r =
+        r.Price
+
+
+    type Bauxite = Bauxite of Resource
+    let rBauxite (Bauxite b) = b    // Unwrapper
+
+    type Chalcopyrite = Chalcopyrite of Resource
+    let rChalc (Chalcopyrite c) = c // Unwrapper
+
+    type Clay = Clay of Resource
+    let rClay (Clay cl) = cl    // Unwrapper
+    let aClay f (Clay cl) = f cl    // Applicator
+
+    type Coal = Coal of Resource
+    let rCoal (Coal c) = c  // Unwrapper
+
+    type CrudeOil = CrudeOil of Resource
+    let rOil (CrudeOil oil) = oil   // Unwrapper
+
+    type GoldOre = GoldOre of Resource
+    let rGOre (GoldOre g) = g   // Unwrapper
+
+    type Gravel = Gravel of Resource
+    let rGravel (Gravel g) = g  //Unwrapper
+    
+    type Ilmenite = Ilmenite of Resource
+    let rIlmenite (Ilmenite ilmn) = ilmn    // Unwrapper
+
+    type IronOre = IronOre of Resource
+    type Limestone = Limestone of Resource
+    type LithiumOre = LithiumOre of Resource
+    type QuartzSand = QuartzSand of Resource
+    type RoughDiamonds = RoughDiamonds of Resource
+    type SilverOre = SilverOre of Resource
+    
+    type Aluminum = Aluminum of Resource
+    type Batteries = Batteries of Resource
+    type Bricks = Bricks of Resource
+    type Concrete = Concrete of Resource
+    type Copper = Copper of Resource
+    type Electronics = Electronics of Resource
+    type Fertilizer = Fertilizer of Resource
+    type FossilFuel = FossilFuel of Resource
+    type Glass = Glass of Resource
+    type Gold = Gold of Resource
+    type Insecticides = Insecticides of Resource
+    type Jewellery = Jewellery of Resource
+    type Lithium = Lithium of Resource
+    type MedicalTechnology = MedicalTechnology of Resource
+    type Plastics = Plastics of Resource
+    type ScanDrones = ScanDrones of Resource
+    type Silicon = Silicon of Resource
+    type Silver = Silver of Resource
+    type Steel = Steel of Resource
+    type Titanium = Titanium of Resource
+    type Trucks = Trucks of Resource
+    type Weapons = Weapons of Resource
