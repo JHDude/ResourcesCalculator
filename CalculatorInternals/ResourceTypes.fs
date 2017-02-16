ca7bb82ab8a6099a1c879ed3cd43a258096feb9a
Jacob Antoni Hassold <jhdude3@gmail.com>
2017-02-15 19:20:46 -0500

    Added beginning of F# code

---
+++ CalculatorInternals/ResourceTypes.fs
@@ -0,0 +1,14 @@
+﻿namespace CalculatorInternals
+
+module ResourceTypes =
+    type BaseResource =
+        | Bauxite
+        | Chalcopyrite
+        | Clay
+        | Coal
+        | CrudeOil
+        | GoldOre
+        | Gravel
+        | Ilmenite
+        | IronOre
+        | Limestone
