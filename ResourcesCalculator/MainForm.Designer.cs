8957a0523388bdecec41c36cefec903f6d96c273
Jacob Antoni Hassold <jhdude3@gmail.com>
2017-02-18 15:38:58 -0500

    More work on the main form

--- ResourcesCalculator/MainForm.Designer.cs
+++ ResourcesCalculator/MainForm.Designer.cs
@@ -28,14 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tbpSupplyChain = new System.Windows.Forms.TabPage();
             this.tbpUserBuildings = new System.Windows.Forms.TabPage();
+            this.ttMain = new System.Windows.Forms.ToolTip(this.components);
+            this.tbpProfits = new System.Windows.Forms.TabPage();
+            this.grbDailyTotals = new System.Windows.Forms.GroupBox();
+            this.txtProfitTotals = new System.Windows.Forms.TextBox();
+            this.lblDailyAll = new System.Windows.Forms.Label();
+            this.txtMines = new System.Windows.Forms.TextBox();
+            this.lblMines = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
+            this.tbpProfits.SuspendLayout();
+            this.grbDailyTotals.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
+            this.tabControl1.Controls.Add(this.tbpProfits);
             this.tabControl1.Controls.Add(this.tbpSupplyChain);
             this.tabControl1.Controls.Add(this.tbpUserBuildings);
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
@@ -64,6 +75,68 @@
             this.tbpUserBuildings.Text = "User Buildings";
             this.tbpUserBuildings.UseVisualStyleBackColor = true;
             // 
+            // tbpProfits
+            // 
+            this.tbpProfits.Controls.Add(this.grbDailyTotals);
+            this.tbpProfits.Location = new System.Drawing.Point(4, 22);
+            this.tbpProfits.Name = "tbpProfits";
+            this.tbpProfits.Size = new System.Drawing.Size(946, 501);
+            this.tbpProfits.TabIndex = 2;
+            this.tbpProfits.Text = "Profitsssssss";
+            this.tbpProfits.UseVisualStyleBackColor = true;
+            // 
+            // grbDailyTotals
+            // 
+            this.grbDailyTotals.Controls.Add(this.txtMines);
+            this.grbDailyTotals.Controls.Add(this.lblMines);
+            this.grbDailyTotals.Controls.Add(this.txtProfitTotals);
+            this.grbDailyTotals.Controls.Add(this.lblDailyAll);
+            this.grbDailyTotals.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
+            this.grbDailyTotals.Location = new System.Drawing.Point(3, 3);
+            this.grbDailyTotals.Name = "grbDailyTotals";
+            this.grbDailyTotals.Size = new System.Drawing.Size(318, 198);
+            this.grbDailyTotals.TabIndex = 1;
+            this.grbDailyTotals.TabStop = false;
+            this.grbDailyTotals.Text = "Daily Totals";
+            // 
+            // txtProfitTotals
+            // 
+            this.txtProfitTotals.Location = new System.Drawing.Point(137, 31);
+            this.txtProfitTotals.Name = "txtProfitTotals";
+            this.txtProfitTotals.ReadOnly = true;
+            this.txtProfitTotals.Size = new System.Drawing.Size(175, 32);
+            this.txtProfitTotals.TabIndex = 1;
+            this.txtProfitTotals.WordWrap = false;
+            // 
+            // lblDailyAll
+            // 
+            this.lblDailyAll.AutoSize = true;
+            this.lblDailyAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblDailyAll.Location = new System.Drawing.Point(6, 41);
+            this.lblDailyAll.Name = "lblDailyAll";
+            this.lblDailyAll.Size = new System.Drawing.Size(31, 17);
+            this.lblDailyAll.TabIndex = 0;
+            this.lblDailyAll.Text = "All:";
+            // 
+            // txtMines
+            // 
+            this.txtMines.Location = new System.Drawing.Point(137, 69);
+            this.txtMines.Name = "txtMines";
+            this.txtMines.ReadOnly = true;
+            this.txtMines.Size = new System.Drawing.Size(175, 32);
+            this.txtMines.TabIndex = 3;
+            this.txtMines.WordWrap = false;
+            // 
+            // lblMines
+            // 
+            this.lblMines.AutoSize = true;
+            this.lblMines.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.lblMines.Location = new System.Drawing.Point(6, 79);
+            this.lblMines.Name = "lblMines";
+            this.lblMines.Size = new System.Drawing.Size(55, 17);
+            this.lblMines.TabIndex = 2;
+            this.lblMines.Text = "Mines:";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -73,6 +146,9 @@
             this.Name = "MainForm";
             this.Text = "Resource Calculator";
             this.tabControl1.ResumeLayout(false);
+            this.tbpProfits.ResumeLayout(false);
+            this.grbDailyTotals.ResumeLayout(false);
+            this.grbDailyTotals.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -82,6 +158,13 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tbpSupplyChain;
         private System.Windows.Forms.TabPage tbpUserBuildings;
+        private System.Windows.Forms.TabPage tbpProfits;
+        private System.Windows.Forms.GroupBox grbDailyTotals;
+        private System.Windows.Forms.TextBox txtMines;
+        private System.Windows.Forms.Label lblMines;
+        private System.Windows.Forms.TextBox txtProfitTotals;
+        private System.Windows.Forms.Label lblDailyAll;
+        private System.Windows.Forms.ToolTip ttMain;
     }
 }
 
