27c2f9eed2c20d1f1b13218dfd43093fd631adca
Jacob Antoni Hassold <jhdude3@gmail.com>
2017-02-18 14:52:39 -0500

    Some comments for those who haven't worked much with F# before

--- CalculatorInternals/Script.fsx
+++ CalculatorInternals/Script.fsx
@@ -10,17 +10,19 @@ open FSharp.Data
 //"ItemID;Item;ItemEN;Kurs;SMKurs"
 
 // Define your library scripting code here
+
+// Defines a CSV provider, and loads the csv from the website
 type R = CsvProvider<"http://www.resources-game.ch/exchange/kurseliste.csv", AssumeMissingValues = true, Separators=";", CacheRows = false, Schema = "int, string, string, int, int, int", HasHeaders = false, SkipRows = 1>
 let RData = R.Load("http://www.resources-game.ch/exchange/kurseliste.csv")
 
+// Type for representing a row of the csv
 type RContainer = {IDNum : int;
                     Item : string;
                     ItemEN : string;
                     MarketPrice : int;
                     HighestBid : int}
                     
-//let DataList = List<RContainer>
-
+// Creates a list of RContainers from the loaded data
 let DataList = [for row in RData.Rows do
                     yield {IDNum = row.Column1;
                             Item = row.Column2;
@@ -28,5 +30,5 @@ let DataList = [for row in RData.Rows do
                             MarketPrice = row.Column4;
                             HighestBid = row.Column5}]
     
-
+// Print out the contents of each row of the list
 List.iter (fun x -> printfn "IDNum = %i, Item = %s, Item(english) = %s, Market Price = $%i, Highest Bid = $%i" x.IDNum x.Item x.ItemEN x.MarketPrice x.HighestBid) DataList
